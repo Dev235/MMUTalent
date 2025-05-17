74b361471544ada242d0e34028225087bbfd214f
Kalla_Deveshwara_Rao <crazydev2003@gmail.com>
2025-05-17 21:11:11 +0800

    General Changes and standardization

--- header.php
+++ header.php
@@ -1,4 +1,18 @@
-    <div class="title-container">
-        <h1 id="title">MMU Talent</h1>
-        <h2>Welcome to MMU Talents Management System</h2>
-    </div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $page_title ?? 'MMU Talent'; ?></title>
+    
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+    
+    <!-- JS -->
+    <script src="js/script.js"></script>
+</head>
+
--- index.php
+++ index.php
@@ -1,30 +1,19 @@
 <?php
-require 'connection.php'; // Include the connection file
-// Check if the connection was successful
+require 'connection.php'; // Your DB connection file
+$page_title = "Index Page"; // Title for the current page
+require 'header.php'; // Header include
 ?>
-
-
-<!DOCTYPE html>
-<html>
-
-<head>
-    <title>Index Page</title>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-    <script src="js/script.js"></script>
-</head>
 
 <body>
-    <?php
-    require 'header.php'; // Include the header file
-    ?>
+    <div class="title-container">
+        <h1 id="title">MMU Talent</h1>
+        <h2>Welcome to MMU Talents Management System</h2>
+    </div>
+    <!-- Page specific content here -->
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br>
-        explicabo amet, quae obcaecati ut dolore dolor maxime impedit adipisci soluta totam suscipit,<br>
+        Explicabo amet, quae obcaecati ut dolore dolor maxime impedit adipisci soluta totam suscipit,<br>
         debitis architecto labore vel quisquam alias repellendus. Harum!</p>
-
-
+    <?php require 'footer.php'; ?>
 </body>
-<?php
-require 'footer.php'; // Include the footer file
-?>
 
 </html>
